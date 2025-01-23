6ed97822deec6f4039bc2a150bcda7989bf9d3a9
Liancohen0104 <liancohen0104@gmail.com>
2025-01-23 21:42:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/recycleview/classes/MyData.java
@@ -0,0 +1,13 @@
+package com.example.recycleview.classes;
+import com.example.recycleview.R;
+
+public class MyData
+{
+    public static String[] nameArray = {"בובספוג", "פטריק", "סקווידויד", "מר קראב", "סנדי", "פלנקטון", "גארי", "פרל", "גברת פאף", "ההולנדי המעופף"}; //שמות הדמויות
+
+    public static String[] descriptionArray = {"ספוג ים שמח ואנרגטי שאוהב לעבוד בסרטן הפריך", "כוכב ים ורוד, החבר הכי טוב של בובספוג", "דיונון מריר שחולם להיות אמן וסובל משכניו", "סרטן חמדן שאוהב כסף ובעל מסעדת הסרטן הפריך", "סנאית חכמה וספורטיבית מטקסס שחיה מתחת למים", "יצור קטן ונבל שמנסה לגנוב את המתכון הסודי של קציצות הסרטן", "חיית המחמד הנאמנה של בובספוג", "בתו של מר קראב, לווייתנית מתבגרת שאוהבת אופנה ומסיבות", "מורת הנהיגה של בובספוג", "רוח רפאים של פיראט מפחיד שנהנה להפחיד את תושבי ביקיני בוטום"}; // תיאורי הדמויות
+
+    public static Integer[] drawableArray = {R.drawable.spongebob, R.drawable.patrick, R.drawable.squidward, R.drawable.mrkrabs, R.drawable.sandy, R.drawable.plankton, R.drawable.gary,R.drawable.pearl, R.drawable.mrspuff, R.drawable.flyingdutchman}; // תמונות
+
+    public static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; // אינדקס של כל תמונה ותיאורה
+}
